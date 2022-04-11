69afbce358e588a0f59ec698bf776b93f4eb6d1c
George Githuma Njogu <george.githuma@gmail.com>
2022-04-12 02:36:22 +0300

    Give image className=product__image in product.js and target it in Product.css

--- src/Product.js
+++ src/Product.js
@@ -29,6 +29,7 @@ function Product() {
 
             {/*Add Product Image*/}
             <img 
+              className='product__image' 
               src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
               alt='Product Image'
             />
