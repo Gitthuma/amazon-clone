add355e4448bbe1c9849382ee6ea3a58bfdfd1c3
George Githuma Njogu <george.githuma@gmail.com>
2022-04-11 15:16:08 +0300

    Create the Home container and add home image

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,15 @@ import './Home.css'
 function Home() {
   return (
     <div className='Home'>
-        <p>I am home component</p>
+        {/*Create the Home container*/}
+        <div className='home__container'>
+            {/*Add home image*/}
+            <img
+                className='home__image'
+                src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
+            />
+
+        </div>
 
     </div>
   )
