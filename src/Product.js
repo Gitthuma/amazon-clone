b181e72c04f88f70671eb722ca17df1cb801e046
George Githuma Njogu <george.githuma@gmail.com>
2022-04-12 01:23:03 +0300

    Add div for product__rating

--- src/Product.js
+++ src/Product.js
@@ -8,20 +8,27 @@ function Product() {
     <div className='product'>
         {/*Create tiles for products__Info*/}
         <div className='product__info'>
+            
             {/*The description*/}
             <p>The lean Start Up</p>
-              {/*Create the product__price paragraph*/}
+              
+            {/*Create the product__price paragraph*/}
             <p className='product__price'>
+              
               {/*Make the dolar sign small and the figures bold with strong*/}
               <small>$</small>
               <strong>19.99</strong>
-            
-
+      
             </p>
-
+            
+            {/*Add div for product__rating*/}
+            <div className='product__rating'>
+              <p>🔆</p>
+              <p>🔆</p>
+              <p>🔆</p>
+            </div>
 
         </div>
-
 
     </div>
   )
