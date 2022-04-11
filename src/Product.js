786e81b9be369e57b1a8b312f5c53fe41057e8cd
George Githuma Njogu <george.githuma@gmail.com>
2022-04-11 15:56:36 +0300

    Created product .js and Product.css to hold products

---
+++ src/Product.js
@@ -0,0 +1,14 @@
+//Create Product component
+import React from 'react'
+//Create and import Product.css
+import './Product.css'
+
+function Product() {
+  return (
+    <div className='product'>
+
+    </div>
+  )
+}
+
+export default Product
