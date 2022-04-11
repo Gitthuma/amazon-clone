695a1f93774e143b2bce32af293a8ef4a8ac603b
George Githuma Njogu <george.githuma@gmail.com>
2022-04-12 01:06:23 +0300

    Create the product__price paragraph and Make the dolar sign small and the figures bold with strong

--- src/Product.js
+++ src/Product.js
@@ -10,6 +10,15 @@ function Product() {
         <div className='product__info'>
             {/*The description*/}
             <p>The lean Start Up</p>
+              {/*Create the product__price paragraph*/}
+            <p className='product__price'>
+              {/*Make the dolar sign small and the figures bold with strong*/}
+              <small>$</small>
+              <strong>19.99</strong>
+            
+
+            </p>
+
 
         </div>
 
